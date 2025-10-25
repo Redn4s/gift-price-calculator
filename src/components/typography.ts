3bc1d70adc0be835348f698991865c40e74cd403
Sander Geenen <sander.geenen@studiohyperdrive.be>
2025-10-26 00:48:39 +0200

    refactor: replace primitives with typography components and update IndexPage layout

---
+++ src/components/typography.ts
@@ -0,0 +1,9 @@
+import { tv } from "tailwind-variants";
+
+export const h1 = tv({
+  base: "text-4xl lg:text-5xl font-semibold",
+});
+
+export const intro = tv({
+  base: "text-lg lg:text-xl",
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-import { subtitle, title } from "@/components/primitives";
+import { h1, intro } from "@/components/typography";
 import { DefaultLayout } from "@/layouts/default";
 
 export const IndexPage = () => {
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Wat Geef Ik?</h1>
-          <h2 className={subtitle()}>
-            Vind het juiste bedrag voor elk cadeau. 🎁
-          </h2>
+      <section className="py-8 md:py-10">
+        <div className="flex flex-col items-center gap-2 md:gap-4">
+          <h1 className={h1()}>Wat Geef Ik?</h1>
+          <p className={intro()}>Vind het juiste bedrag voor elk cadeau. 🎁</p>
         </div>
       </section>
     </DefaultLayout>
